f78bbf68293a18654c1de16307364b1bb0c97d0e
sanglailai <146155698+sanglailai@users.noreply.github.com>
2025-03-25 18:18:40 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,22 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  website: "https://astro-clean.vercel.app", // 或等你部署后填入自己的域名
+  author: "Lailai",
+  profile: "",
+  desc: "眨眼就过了",
+  title: "Lailai's Blog",
+  ogImage: "https://emojicdn.elk.sh/🐶", // emoji 风格头像
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  scheduledPostMargin: 15 * 60 * 1000,
   showArchives: true,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
   editPost: {
     enabled: true,
     text: "Suggest Changes",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/sanglailai/astro-clean/edit/main/",
   },
   dynamicOgImage: true,
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "zh",
+  timezone: "Asia/Shanghai",
 } as const;
